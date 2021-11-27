e8917d0131cfddc6a013d51c3b895f70f59f102d
Oz Tamir <theoztamir@gmail.com>
2021-11-27 13:57:37 +0000

    Moved code into src folder

--- config.js
+++
@@ -1,7 +0,0 @@
-const API_TOKEN = "TOKEN"
-const API_ENDPOINT = "https://MINISTERY_OF_TRANSPORTATION_ENDPOINT/"
-
-module.exports = {
-    token : API_TOKEN,
-    endpoint : API_ENDPOINT
-};
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const API_TOKEN = "DM1234"
+const API_ENDPOINT = "http://<base_endpoint>/2.8/json"
+
+module.exports = {
+    token : API_TOKEN,
+    endpoint : API_ENDPOINT
+};
