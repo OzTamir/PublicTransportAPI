2674c1ff5f084f7317e38ad4961aea94f8966db2
Oz Tamir <theoztamir@gmail.com>
2021-11-27 09:54:04 +0000

    Added API for station information

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 'use strict';
 const config = require('./config');
 const express = require('express');
+const got = require('got');
+
+function getStationInformation(stationId) {
+  console.log('getStationInformation:', stationId);  
+  return got(`${config.endpoint}?Key=${config.token}&MonitoringRef=${stationId}`).then(function(response) {
+    const json = JSON.parse(response.body);
+    return JSON.stringify({
+      id: stationId,
+      stops: json["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]
+    });
+  });
+}
 
 // Constants
 const PORT = 8080;
@@ -8,9 +20,17 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('API Token: ' + config.token
     + '\nAPI Endpoint: ' + config.endpoint);
+});
+
+app.get('/station/:stationId', (req, res) => {
+  const stationId = req.params.stationId;
+  getStationInformation(stationId).then(data => {
+      res.send(data);
+  });
 });
 
 app.listen(PORT, HOST);
