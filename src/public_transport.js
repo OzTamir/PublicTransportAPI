d7c41a747d2dc028c51daa2a065d7bc3a701e81f
Oz Tamir <theoztamir@gmail.com>
2021-11-27 14:06:52 +0000

    Avoid leaking secrets and better debug log

--- src/public_transport.js
+++ src/public_transport.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const got = require('got');
-const config = require('./config');
+const config = require('./config.js');
 
 function getRawStationInformation(stationId, callback) {
-    console.log('getRawStationInformation:', stationId);
     return got(`${config.endpoint}?Key=${config.token}&MonitoringRef=${stationId}`).then(function(response) {
       return callback(JSON.parse(response.body));
     });
 }
 
 function getStationInformation(stationId) {
-    console.log('getStationInformation:', stationId);  
     return getRawStationInformation(stationId, function(json) {
         return JSON.stringify({
             id: stationId,
@@ -29,7 +27,6 @@ function calculateTimeToStation(recordTime, expectedArrivalTime) {
 };
 
 function getTimeToStation(stationId, lineId) {
-    console.log('getTimeToStation:', stationId, lineId);
     return getRawStationInformation(stationId, function(json) {
         const stops = json["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"];
         var lineInfo = [];
